d88c16e3d33d684eeb5fd7ff06b01edd42b5dfea
Danny <m.r.fouladi7@gmail.com>
2025-11-10 13:21:05 +0330

    Infrastructure service container and presentation settings

---
+++ AuthenticationApi.Infrastructure/DependencyInjection/InfrastructureServiceContainer.cs
@@ -0,0 +1,29 @@
+﻿using AuthenticationApi.Application.Interfaces;
+using AuthenticationApi.Infrastructure.Data;
+using AuthenticationApi.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Resource.Share.Lib.DependencyInjection;
+
+namespace AuthenticationApi.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            ShareServiceContainer.AddShareService<AuthenticationDbContext>(services, config, config["AppSerilog:FileName"]!);
+
+            services.AddScoped<IUser, UserRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            ShareServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
